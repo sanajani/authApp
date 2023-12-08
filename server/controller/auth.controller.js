1003b6c8b880f89361270e44c345f1c2507578bc
Sanaullah <webdevfarsi@gmail.com>
2023-12-08 22:30:59 +0430

    signin for server side is done

--- server/controller/auth.controller.js
+++ server/controller/auth.controller.js
@@ -1,6 +1,7 @@
 import userModel from '../models/user.model.js'
 import { customError } from '../utils/custom.error.js';
 import bcryptjs from 'bcryptjs'
+import jwt from 'jsonwebtoken'
 
 export const signup = async (req,res,next) => {
     const {username,email,password} = req.body;
@@ -25,3 +26,20 @@ export const signup = async (req,res,next) => {
     }
 }
 
+export const signin = async (req,res,next) => {
+    const {email,password} = req.body;
+
+    try {
+    const validUser = await userModel.findOne({email})
+    if(!validUser) return next(customError(404,"User not found"))  
+    const validPassword = bcryptjs.compareSync(password,validUser.password)
+    if(!validPassword) return next(customError(401,"Invalid credentials"))
+    const token = jwt.sign({id:validUser._id},process.env.JWT_SECRET)
+    // validUser.password = ''
+    const {password:hashPassword, ...rest} = validUser._doc
+    const expiryDate = new Date(Date.now() + 3600000)
+    res.cookie('access_token',token,{httpOnly:true, expires: expiryDate}).status(201).json(rest)
+    } catch (error) {
+        next(error)
+    }
+}
