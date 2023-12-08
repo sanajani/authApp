b6ed3879760da629456ce5a6321509d8b1b9da1f
Sanaullah <webdevfarsi@gmail.com>
2023-12-08 17:48:36 +0430

    user router file is created

--- server/index.js
+++ server/index.js
@@ -2,12 +2,20 @@ import dotenv from 'dotenv'
 dotenv.config()
 import express from 'express'
 
+// 
+
 // import database
 import connection from './db/connect.js';
 connection();
 
 const app = express();
 const PORT = process.env.PORT || 9011
+
+app.get('/',(req,res) => {
+    res.json({
+        message:"Api called"
+    })
+})
 
 app.listen(PORT,() => {
     console.log(`server is running on http://localhost:${PORT}`);
